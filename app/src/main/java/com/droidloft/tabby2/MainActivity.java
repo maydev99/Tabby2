636c46510d2ae9617d743eb9ed3e69c38b2eb5e6
Michael May <maydev99@gmail.com>
2017-03-05 09:34:05 -0500

    Added Multiple Layouts

--- app/src/main/java/com/droidloft/tabby2/MainActivity.java
+++ app/src/main/java/com/droidloft/tabby2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.droidloft.tabby2;
 
+import android.media.Image;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -11,12 +12,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private SectionPagerAdapter mSectionPagerAdapter;
     private ViewPager mViewPager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         mViewPager = (ViewPager)findViewById(R.id.container);
         mViewPager.setAdapter(mSectionPagerAdapter);
+
+
     }
 
     public static class PlaceHolderFragment extends Fragment {
@@ -49,11 +55,39 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = null;
+
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+                case 1: {
+                    rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                    break;
+                }
+
+                case 2: {
+                    rootView = inflater.inflate(R.layout.fragment2, container, false);
+                    break;
+
+                }
+
+                case 3: {
+                    rootView = inflater.inflate(R.layout.fragment3, container, false);
+                    break;
+                }
+
+                case  4: {
+                    rootView = inflater.inflate(R.layout.fragment4, container, false);
+                    break;
+                }
+            }
+
+            int[] pics= {R.drawable.mike, R.drawable.cate, R.drawable.steve, R.drawable.gwen};
             TextView textView = (TextView) rootView.findViewById(R.id.section_number);
+            ImageView imageView = (ImageView)rootView.findViewById(R.id.imageView);
+            imageView.setImageResource(pics[getArguments().getInt(ARG_SECTION_NUMBER)-1]);
             textView.setText("" + getArguments().getInt(ARG_SECTION_NUMBER));
             return rootView;
         }
+
     }
 
 
@@ -70,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 3;
+            return 4;
         }
 
         public  CharSequence getPageTitle(int position){
@@ -81,6 +115,8 @@ public class MainActivity extends AppCompatActivity {
                     return "SECTION 2";
                 case  2:
                     return "SECTION 3";
+                case  3:
+                    return "SECTION 4";
             }
 
             return null;
